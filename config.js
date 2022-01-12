b6832bcb865fe5a7ed7e2ff6e30801b5c11c7947
Adwaithkrishna2008 <kichuadwaith28@gmail.com>
2022-01-12 19:11:30 +0530

    configuration file has been added

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAvM0yWq8twR8OYHYKz4dpclmKh6qSFDcA",
+    authDomain: "vote-c-60.firebaseapp.com",
+    databaseURL: "https://vote-c-60-default-rtdb.firebaseio.com",
+    projectId: "vote-c-60",
+    storageBucket: "vote-c-60.appspot.com",
+    messagingSenderId: "566774748499",
+    appId: "1:566774748499:web:9aa7c2bd39409b0d217da6"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
